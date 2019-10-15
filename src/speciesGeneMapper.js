67f31caa23cce51fc1e86f344ed341370d1f0771
Jordan Douglas <jordandouglas@windowslive.com>
2019-10-16 11:43:48 +1300

    Automatic gene to species mapper

--- src/speciesGeneMapper.js
+++ src/speciesGeneMapper.js
@@ -26,14 +26,187 @@
 
 
 
-// Attempts to map a gene tree to a species tree, or returns an error message
-function mapGeneTreeToSpeciesTree(geneTree, speciesTree){
-	
-	
-	
-	
-	
+
+function initMapper(){
+
+
+
+
+	var splitBy = function(speciesLabel, geneLabel, index, delimiter) {
+
+		if (index < 0) return null;
+		var geneBits = geneLabel.split(delimiter);
+		if (index >=  geneBits.length) return null;
+		return geneBits[index] == speciesLabel;
+
+	}
+
+
+	var strContains = function(speciesLabel, geneLabel, index) {
+		if (index != 0) return null;
+		return geneLabel.includes(speciesLabel);
+
+	}
+
+
+	// A list of functions to determine whether species label s can be mapped to g, for some parameter i
+	MAPPING_STRATEGIES = [	function(s, g, i) { return strContains(s, g, i); }, 
+				function(s, g, i) { return strContains(s.toLowerCase(), g.toLowerCase(), i); }, 
+				function(s, g, i) { return splitBy(s, g, i, "_"); }, 
+				function(s, g, i) { return splitBy(s, g, i, "-"); },
+				function(s, g, i) { return splitBy(s, g, i, "."); },
+				function(s, g, i) { return splitBy(s.toLowerCase(), g.toLowerCase(), i, "_"); },
+				function(s, g, i) { return splitBy(s.toLowerCase(), g.toLowerCase(), i, "-"); },
+				function(s, g, i) { return splitBy(s.toLowerCase(), g.toLowerCase(), i, "."); }
+			];
+
+
+
 }
+
+
+
+
+// Attempts to map a gene tree to a species tree, or returns an error message
+function mapGeneTreeToSpeciesTree(g, treename, geneLeaves, speciesLeaves){
+	
+
+
+	// For each mapping strategy
+	var mappedSuccessfully = false;
+	for (var st = 0; st < MAPPING_STRATEGIES.length; st ++){
+
+		var strategy = MAPPING_STRATEGIES[st];
+		var strategyWorks = false;
+		
+
+		// Parameters
+		var strategyParameter = 0;
+		var parameterInRange = true;
+		while (parameterInRange) {
+
+			var parameterStrategyWorks = true;
+
+			// For each gene taxa
+			for (var j = 0; j < geneLeaves.length; j ++) {
+				var gene_leaf = geneLeaves[j];
+				var geneMappedTo = [];
+
+				// Try to map this gene taxon to a species taxon
+				for (var i = 0; i < speciesLeaves.length; i ++){
+					var species_leaf = speciesLeaves[i];
+
+
+					var mappingAttempt = strategy(species_leaf.label, gene_leaf.label, strategyParameter);
+					if (mappingAttempt == null) {
+
+						// Parameter out of range
+						parameterInRange = false;
+						break;
+
+					}
+
+					if (mappingAttempt) {
+
+						// Mapped
+						geneMappedTo.push(species_leaf);
+						if (geneMappedTo.length > 1) break;
+
+
+					}
+
+
+
+				}
+
+
+				if (!parameterInRange) {
+					parameterStrategyWorks = false;
+					break;
+				}
+
+
+				// Check that this gene taxon can be mapped to 1 and only 1 species taxon
+				if (geneMappedTo.length == 1){
+
+					console.log(gene_leaf.label, "mapped to", species_leaf.label);
+					var species_leaf = geneMappedTo[0];
+					species_leaf.branchToGeneNodeMap[g][gene_leaf.id] = gene_leaf;
+					species_leaf.nodeToGeneBranchMap[g][gene_leaf.id] = gene_leaf;
+					gene_leaf.speciesNodeMap = species_leaf;
+
+
+				}else{
+
+					// Strategy did not work for this parameter
+					parameterStrategyWorks = false;
+					console.log("Strategy", st, strategyParameter, "did not work for", gene_leaf.label, "|", geneMappedTo.length );
+					break;
+				}
+
+
+
+			}
+
+
+			if (parameterStrategyWorks) {
+				strategyWorks = true;
+				break;
+			}
+			
+
+
+			// Try again with the next parameter value
+			strategyParameter++;
+			
+			
+			
+		}
+
+
+
+		if (strategyWorks) {
+			console.log("Strategy", st, strategyParameter, "works");
+			mappedSuccessfully = true;
+			break;
+		}
+
+
+	}
+
+
+	if (mappedSuccessfully) {
+		return true;
+
+	}else{
+		$("#geneTreeUploadMsg").append("<b>Unable to automatically map " + treename + ` to the species tree.</b>  Please ensure that the gene tree leaf labels contain the
+			species leaf labels they are mapped to (perhaps delimited by '_', '-', or '.'), and ensure that each gene taxon maps to only one species taxon.`);
+		return false;
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
