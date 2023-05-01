83296c767e7e406a5a3ce9b94aba577b071e16cc
jordandouglas <jordandouglas@windowslive.com>
2023-05-01 13:30:52 +1200

    rudimentary screen recorder

--- src/recorder.js
+++ src/recorder.js
@@ -1,270 +1,375 @@
+
+
+
 /*
-	
-	MIT License
-
-	Copyright (c) 2019 UglyTrees
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
-
+* Initialise the recorder
 */
+function initRecorder(startBtnID, stopBtnID, cancelBtnID, progressID) {
 
 
-function initRecorder() {
-
-
-
-	ANIMTATION_TIME_RECORDER = 1000;
-	FADE_TIME_RECORDER = 1000;
+	//ANIMTATION_TIME_RECORDER = 1000;
+	//FADE_TIME_RECORDER = 1000;
+	RECORDER_COUNTDOWN_EVERY = 750;
+	RECORDER_FPS = 50;  	// Fames per second when recording 
+	RECORDER_DELAY = 50;    // ms between frames when combining into gif
 	RECORDING = false;
-	recorder = null;
+	ENCODING = false;
+	RECORDER_START_BTN = startBtnID;
+	RECORDER_STOP_BTN = stopBtnID;
+	RECORDER_CANCEL_BTN = cancelBtnID;
+	RECORDER_PROGRESS_ID = progressID;
+	//recorder = null;
+
+
+	$("#" + RECORDER_START_BTN).show();
+	$("#" + RECORDER_STOP_BTN).hide();
+	$("#" + RECORDER_CANCEL_BTN).hide();
+	$("#" + RECORDER_PROGRESS_ID).hide();``
 
 }
 
 
 
 
-function record(svgID = "tree"){
+/*
+* Initialise the recorder and record until stop is called
+*/
+function startRecording(svgID = "tree"){
 
 
-	
 
-
+	// Start recording the svg
 	RECORDING = true;
-
-	$("#treeDIV").hide(0);
-	CURRENT_ANIMATION_TIME = ANIMTATION_TIME_RECORDER;
-	CURRENT_FADE_TIME = FADE_TIME_RECORDER;
-
-	// The svg
-	var svg = document.getElementById(svgID);
-	var svg_jq = $("#" + svgID);
-	var svg_width = parseFloat(svg_jq.width());
-	var svg_height = parseFloat(svg_jq.height());
-	var canvasSizeMultiplier = 1.5;
-
-	// Ensure that height and width are inline and not css
-	svg_jq.attr("height", svg_height);
-	svg_jq.attr("width", svg_width);
+	ENCODING = false;
+	$("#" + RECORDER_START_BTN).hide();
+	$("#" + RECORDER_STOP_BTN).show();
+	$("#" + RECORDER_CANCEL_BTN).hide();
+	$("#" + RECORDER_PROGRESS_ID).hide();
 
 
-	// The canvas
-	var canvas = document.createElement("canvas");
-	canvas.width = svg_width * canvasSizeMultiplier; 
-	canvas.height = svg_height * canvasSizeMultiplier; 
-	var context = canvas.getContext("2d");
-	context.fillStyle = "white";
 
-	// The recorder
-	var data = [];
+
+	// Create a div to store the pngs
+	$("#recorderPngBox").remove();
+	let pngBox = $("<div style='display:none' id='recorderPngBox'>Hello</div>");
+	$("#treeDIV").prepend(pngBox);
+
 
 	
-    	var stream = canvas.captureStream(0);
-    	recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
-	recorder.ondataavailable = function(event) {
-		//console.log("data?", event);
-		if (event.data && event.data.size) {
-			data.push(event.data);
+
+	// Start the countdown
+	let countdownEle = $("<div style='position:absolute; z-index:100; top:40%; left: 50%; font-size:10em; color:white'>HELLO</div>");
+	countdownEle.css("textShadow", "1px -1px 5px #000000, 1px 1px 5px #000000, -1px -1px 5px #000000, -1px 1px 5px #000000");
+	$("body").append(countdownEle);
+	startCountdown(countdownEle, 5, function(){
+		recordRecursive($("#" + svgID), pngBox);
+	});
+	
+
+
+}
+
+
+/*
+* Start the timer recording countdown
+*/
+function startCountdown(countdownEle, timerVal = 4, resolve = function() { }){
+
+	console.log(timerVal);
+
+	countdownEle.html(timerVal);
+
+	setTimeout(function() {
+
+		
+
+		if (timerVal <= 1){
+			countdownEle.fadeOut(0, function(){
+				countdownEle.remove();
+			});
+			resolve();
+			return;
 		}
-	};
+
+		startCountdown(countdownEle, timerVal-1, resolve);
+
+	}, RECORDER_COUNTDOWN_EVERY); 
+
+}
 
 
+/*
+* Recursively record until told to stop
+*/
+function recordRecursive(svg, pngBox){
+
+
+
+	// Every 1/FPS seconds, sample a png
+	setTimeout(function() {
+
+
+		// Finish the recording and make a gif
+		if (!RECORDING) {
+			makeGif(pngBox);
+			return;
+		}
+
+		console.log("recording...");
+
+
+		// Pause the animation
+		svg.velocity("pause");
+
+		// Draw the png
+		makePng(svg, pngBox, function(){
+
+
+			// Resume the animation
+			svg.velocity("resume");
+			recordRecursive(svg, pngBox);
+
+		});
+
+
+
+
+	}, 1000.0/RECORDER_FPS);
+
+}
+
+
+/*
+* Stop recoding the animation
+*/
+function stopRecording(){
+
+	// Stop recording the svg
+	RECORDING = false;
+	ENCODING = false;
+	$("#" + RECORDER_START_BTN).show();
+	$("#" + RECORDER_STOP_BTN).hide();
+	$("#" + RECORDER_CANCEL_BTN).hide();
+	$("#" + RECORDER_PROGRESS_ID).hide();
+	$("#" + RECORDER_PROGRESS_ID).children("span").html("");
+
+}
+
+
+
+/*
+* Cancel the gif encoding
+*/
+function stopEncoding(){
+
+	// Stop encoding the svg
+	ENCODING = false;
+
+}
+
+
+
+/*
+* Combine a series of pngs into a gif and download it, using the jsgif library
+* https://github.com/antimatter15/jsgif
+*/
+function makeGif(pngBox){
+
+
+	$("#" + RECORDER_START_BTN).hide();
+	$("#" + RECORDER_CANCEL_BTN).show();
+	$("#" + RECORDER_PROGRESS_ID).show();
+	ENCODING = true;
+
+
+
+	// Provide some feedback to the user about how long it is taking
+	let dialog = $("#" + RECORDER_PROGRESS_ID).children("span");
+	//$("")
+
+
+
+	// Initialise the encoder
+	let encoder = new GIFEncoder();
+	encoder.setRepeat(0);
+  	encoder.setDelay(RECORDER_DELAY);
+  	encoder.start();
+
+
+  	console.log("initialised");
+  	//return;
+
+
+	 // Iterate through all the canvases
+	 let canvases = pngBox.children("canvas");
+
+	 encodeFrame(0, encoder, dialog, canvases, function(){
+
+	 	// Finalise
+		dialog.html( "100%");
+	 	console.log("finalising");
+
+
+		// Finalise animation
+	    encoder.finish();
+	  	//let binary_gif = encoder.stream().getData() 
+	  	//let data_url = 'data:image/gif;base64,'+encode64(binary_gif);
+
+
+	  	encoder.download("animation.gif");
+	  	stopRecording();
+
+	  	$(pngBox).remove();
+
+	 });
 
 	
 
-  	recorder.onstop = () => {
 
-		//return;
-		var blob = new Blob(data, { type: "video/webm" })
-	   	var url = URL.createObjectURL(blob);
-		console.log("STOP", blob);
+}
 
 
-		var downloadLink = document.createElement("a");
+
+/*
+* Prepare one frame with the encoder	
+*/
+function encodeFrame(frameNum, encoder, dialog, canvases, resolve = function() { }){
+
+	// Cancel
+ 	if (!ENCODING){
+ 		stopRecording();
+ 		return;
+ 	}
+
+ 	// Finalise
+ 	if (frameNum >= canvases.length){
+
+ 		resolve();
+	  	return;
+
+ 	}
+
+
+ 	// Give feedback to user
+ 	dialog.html( Math.round(100.0 * frameNum/canvases.length) + "%");
+ 	let context = $(canvases[frameNum])[0].getContext('2d');
+
+
+ 
+
+
+ 	// Async recurse
+ 	setTimeout(function(){
+
+		// Add frame
+ 		encoder.addFrame(context);
+ 		encodeFrame(frameNum+1, encoder, dialog, canvases, resolve);
+ 	}, 1);
+
+}
+
+
+/*
+	Make a canvas from an svg
+*/
+function makePng(inputSvg, outputDiv, resolve = function() { }){
+	
+	
+	const SCALE_BY = 1;
+	inputSvg = $(inputSvg);
+	
+
+	// Clone svg
+	let cloneSVG = $(inputSvg).clone();
+	outputDiv.append(cloneSVG);
+
+	// Scale width and height. Apsect ratio will be preserved by viewBox
+	cloneSVG.attr("xmlns", "http://www.w3.org/2000/svg");
+	const w = parseFloat(cloneSVG.css("width"))*SCALE_BY;
+	const h = parseFloat(cloneSVG.css("height"))*SCALE_BY;
+	cloneSVG.attr("width", w);
+	cloneSVG.attr("height", h);
+	//cloneSVG.width(w);
+	//cloneSVG.height(h);
+	cloneSVG.removeClass("width");
+	cloneSVG.removeClass("height");
+
+
+	
+	
+	if (w*h > 2e8){
+		//inputSvg.parentNode.removeChild(elem);
+		alert("Maximum png size exceeded!");
+		return;
+	}
+	
+	// Generate blob for png
+	let svgStr = $(cloneSVG).prop('outerHTML');
+
+
+	
+	// Create a blank canvas
+	let canvas = document.createElement("canvas"); // create <canvas> element
+	$(outputDiv).append(canvas);
+    canvas.width = w;
+    canvas.height = h;
+	
+    let context = canvas.getContext("2d");
+	
+    let image = new Image; // create <img> element
+	
+	
+	
+	// Add canvas to page
+	image.onload = function () {
+		
+		// White bg
+		context.fillStyle = "white";
+		context.fillRect(0, 0, w, h); 
+		
+		// Draw figure
+		context.drawImage(image, 0, 0);
+
+
+		cloneSVG.remove();
+		
+		//let url = canvas.toDataURL("image/png");
+		resolve();
+
+	  
+
+		/*
+		//console.log(url);
+		let downloadLink = document.createElement("a");
 		downloadLink.href = url;
-		downloadLink.download = "UglyTrees.webm";
+		downloadLink.download = name;
 		document.body.appendChild(downloadLink);
 		downloadLink.click();
 		document.body.removeChild(downloadLink);
-
-		CURRENT_ANIMATION_TIME = ANIMATION_TIME;
-		CURRENT_FADE_TIME = FADE_TIME;
-		$("#treeDIV").show(0);
-		RECORDING = false;
-
-		console.log("ndraws", ndraws);
+		*/
+		
+    }.bind(this);
 
 
-  	};
+
+    
+    // btoa — binary string to ASCII (Base64-encoded)
+    image.src = 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(svgStr))); 
 	
 
-	var ndraws = 0;
-
-	var drawFrame = function(cb = function(a, b) { }) {
-		var img = new Image();
-		var serialized = new XMLSerializer().serializeToString(svg); 
-		var blob = URL.createObjectURL(new Blob([serialized], {type: "image/svg+xml"}));
-		img.src = blob;
-		img.onload = function() {
-			context.fillRect(0, 0, svg_width * canvasSizeMultiplier, svg_height * canvasSizeMultiplier); 
-			context.drawImage(img, 0, 0, svg_width * canvasSizeMultiplier, svg_height * canvasSizeMultiplier);
-			ndraws++;
-			stream.requestFrame();
-			recorder.requestData();
-			cb(null, img);
-			
-		}
-	    	
-	  }
+ 
 
 
-
-
-
-	recorder.start();
-	recordRecurse(5, recorder, data, drawFrame);
-
-	return; 
 	
-
 }
+
+
+
 
 
 function recordRecurse(n, recorder, data, drawFrame = function(cb) { }){
 
 
-	if (n == 0) {
-		recorder.stop();
-		return;
-
-	}
-
-	recorder.pause();
-	drawFrame(function(a, b) {
-
-		
-		nextTree(false, function() { }, function(elements, complete, remaining, start, tweenValue) {
-			recorder.resume();
-			console.log((complete * 100) + "%");
-        		//console.log(remaining + "ms remaining!");
-        		//console.log("The current tween value is " + tweenValue)
-
-			$(`[animatable="true"]`).velocity("pause");
-			
-			
-			drawFrame(function(a, b) { 
-				console.log("drawn");
-				recorder.pause();
-				setTimeout(function() { 
-					
-					$(`[animatable="true"]`).velocity("resume") 
-				}, 10); 
-			});
-
-			if (complete == 1) {
-				setTimeout(function() {
-					
-					recordRecurse(n-1, recorder, data, drawFrame);					
-				}, 100);
-			}
-		});
-
-	});
-
-
-}
-
-
-
-
-
-
-
-
-// Begin recording the svg 
-// https://gist.github.com/veltman/ff864215009174bc5d164ec3533125c2
-function recoadadrd(svgSelector = "#svgtest"){
-
-		var svg = d3.select(svgSelector),
-	    canvas = document.createElement("canvas"),
-	    width = canvas.width = +svg.attr("width"),
-	    height = canvas.height = +svg.attr("height"),
-	    context = canvas.getContext("2d");
-	var projection = d3.geoOrthographic()
-	  .scale(195)
-	  .translate([width / 2, height / 2])
-	  .precision(0.1);
-	var path = d3.geoPath().projection(projection);
-	  var data = [],
-	    stream = canvas.captureStream(),
-	    recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
-	  recorder.ondataavailable = function(event) {	
-		console.log("RECORD", event);
-	    if (event.data && event.data.size) {
-	      data.push(event.data);
-	    }
-	  };
-	  recorder.onstop = () => {
-		console.log("STOP", data);
-	    var url = URL.createObjectURL(new Blob(data, { type: "video/webm" }));
-	    d3.selectAll("canvas, svg").remove();
-	    d3.select("body")
-	      .append("video")
-	      .attr("src", url)
-	      .attr("controls", true)
-	      .attr("autoplay", true);
-	  };
-	  var background = svg.append("rect")
-	    .attr("width", width)
-	    .attr("height", height)
-	    .attr("fill", "#fff");
-	  svg.append("path")
-	    .datum({ type: "Sphere" })
-	    .attr("stroke", "#222")
-	    .attr("fill", "none");
-	 
-	  var queue = d3.queue(1);
-	  d3.range(120).forEach(function(frame){
-	    queue.defer(drawFrame, frame / 120);
-	  });
-	  queue.awaitAll(function(err, frames){
-	    recorder.start();
-	    drawFrame();
-	    function drawFrame() {
-	      if (frames.length) {
-		context.drawImage(frames.shift(), 0, 0, width, height);
-		requestAnimationFrame(drawFrame);
-	      } else {
-		recorder.stop();
-	      }
-	    }
-	  });
-	  function drawFrame(t, cb) {
-	    projection.rotate([360 * t]);
-	    svg.selectAll("path").attr("d", path);
-	    var img = new Image(),
-		serialized = new XMLSerializer().serializeToString(svg.node()),
-		url = URL.createObjectURL(new Blob([serialized], {type: "image/svg+xml"}));
-	    img.onload = function(){
-	      cb(null, img);
-	    };
-	    img.src = url;
-	  }
 
 
 }
